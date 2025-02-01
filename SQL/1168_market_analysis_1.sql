fa4ca3ae52762ae5b68f95c7a85d5a30c22d437e
Syed Zeeshan Ahmed <syedzeeshanahmed@live.in>
2025-02-01 19:12:44 -0400

    market analysis

---
+++ SQL/1168_market_analysis_1.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select u.user_id as buyer_id
+    , u.join_date
+    , count(o.order_id)  as orders_in_2019
+from Users u left join Orders o 
+    on u.user_id = o.buyer_id and Year(o.order_date) = 2019 
+group by u.user_id
